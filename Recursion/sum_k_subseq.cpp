27da4890bf445a5376bda5743403a950f764ba6f
Sarvesh <sarveshvaradkar123@gmail.com>
2024-12-25 18:22:39 +0530

    updated code for only one subsequesce with sum k

--- Recursion/sum_k_subseq.cpp
+++ Recursion/sum_k_subseq.cpp
@@ -2,26 +2,36 @@
 #include<vector>
 using namespace std;
 
-void sumk(int ind,vector<int> &A,vector<int> &ds,int n,int k,int sum){ 
+
+// updated code for only one subsequesce with sum k
+bool sumk(int ind,vector<int> &A,vector<int> &ds,int n,int k,int sum){ 
     if(ind==n){
         if(k==sum){
             for(auto it:ds){
                 cout<<it<<" ";
             }
             cout<<endl;
+
+            return true;
         }
-        return;
+        return false;
     }
+
+
 
     //pick condition
     ds.push_back(A[ind]);
     sum+=A[ind];
-    sumk(ind+1,A,ds,n,k,sum);
+    if(sumk(ind+1,A,ds,n,k,sum)){
+        return true;
+    }
     sum-=A[ind];
     ds.pop_back();
 
     //non pick condition
-    sumk(ind+1,A,ds,n,k,sum);
+    if(sumk(ind+1,A,ds,n,k,sum))  return true;
+
+    return false;
 
 }
 
