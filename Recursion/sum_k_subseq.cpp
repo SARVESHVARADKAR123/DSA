e2d57047527bcd4a645d50134d5ef1a3fdb460b7
Sarvesh <sarveshvaradkar123@gmail.com>
2024-12-25 14:18:23 +0530

    sum_k

---
+++ Recursion/sum_k_subseq.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sumk(int ind,vector<int> &A,vector<int> &ds,int n,int k,int sum){ 
+    if(ind==n){
+        if(k==sum){
+            for(auto it:ds){
+                cout<<it<<" ";
+            }
+            cout<<endl;
+        }
+        return;
+    }
+
+    //pick condition
+    ds.push_back(A[ind]);
+    sumk(ind+1,A,ds,n,k,sum+A[ind]);
+    ds.pop_back();
+
+    //non pick condition
+    sumk(ind+1,A,ds,n,k,sum);
+
+}
+
+
+int main(){
+    
+    vector<int> A = {1,2,3,3,5};
+    vector<int> ds;
+
+    sumk(0,A,ds,A.size(),6,0);
+    return 0;
+}
